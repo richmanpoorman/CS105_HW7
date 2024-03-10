8ff8a75b5b6135e9b2e9a6203773ad6b604d6c4c
Kaelen Song <126536425+kaelensong@users.noreply.github.com>
2024-03-09 21:51:48 -0500

    regression let

--- hw07/regression.scm
+++ hw07/regression.scm
@@ -78,6 +78,15 @@
 ;; (check-type-error (let ([x : int] [y : bool]) (or x y)))
 (check-type-error (let ((x #t) (y #f))  (+ x y)))
 
+;; step 12
+    ;; Lambda
+    ;; (lambda ([x1 : tyex1] … [xn : tyexn]) e)
+;; (check-type (lambda (x y) (+ x y)) int)
+;; (check-type (lambda ([x : bool] [y : bool]) (or x y)) bool)
+;; (check-type-error (lambda (x y) (+ x y)))
+;; (check-type-error (lambda ([x : bool] [y : int]) (or x y)))
+
+
 ;; step 14 
     ;; Let STAR!
 (check-type (let* ((x 1) (y 2)) (+ x y)) int)
@@ -90,7 +99,10 @@
     ;; (letrec [([x1 : tyex1] e1)
     ;;      … 
     ;;      ([xn : tyexn] en)] e)
-(check-type (letrec ((x 1) (y 2)) (+ x y)) int)
-(check-type (letrec ((x #t) (y #f)) (if y x y)) bool)
-;; (check-type-error (letrec ([x : int] [y : bool]) (or x y)))
-(check-type-error (letrec ((x #t) (y #f))  (+ x y)))
+
+;;     (letrec [([x1 : int] (lambda () x1))] (+ x1 x2))
+
+;; ;; (check-type (letrec (([x : int] [y : int]) (+ x y))) int)
+;; ;; ;; (check-type (letrec ([x : bool] [y : bool]) (if y x y)) bool)
+;; ;; ;; (check-type-error (letrec ([x : int] [y : bool]) (or x y)))
+;; ;; (check-type-error (letrec ([x : int] [y : bool]) (+ x y)))
